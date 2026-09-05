9087b376dfeec014d3c4810bd9a9c48231046bcb
Bmorris25 <brittany2morris@gmail.com>
2026-09-05 14:01:36 -0700

    structure months. Just add questions

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -1,9 +1,25 @@
-import { octoberQuiz } from "./october";
 import { septemberQuiz } from "./september";
+import { octoberQuiz } from "./october";
+import { novemberQuiz } from "./november";
+import { decemberQuiz } from "./december";
+import { januaryQuiz } from "./january";
+import { februaryQuiz } from "./february";
+import { marchQuiz } from "./march";
+import { aprilQuiz } from "./april";
+import { mayQuiz } from "./may";
+import { juneQuiz } from "./june";
 
 
 export const quizzes = {
   September: septemberQuiz,
   October: octoberQuiz,
+  November: novemberQuiz,
+  December: decemberQuiz,
+  January: januaryQuiz,
+  February: februaryQuiz,
+  March: marchQuiz,
+  April: aprilQuiz,
+  May: mayQuiz,
+  June: juneQuiz
   
 };
